9c694df456247dddbad615645726675cd98b8c1c
Zirod Judi <115097584+Zirod@users.noreply.github.com>
2026-03-24 12:26:43 +0000

    Remplacement par tous le projet complet

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+
+        <h1>Zirod Judi</h1>
+        <h5>Full-stack developer specializing in scalable web applications with : Next.js, Node.js, Fastify, Prisma, and PostgreSQL</h5>
+
+      </main>
+    </div>
+  );
+}
